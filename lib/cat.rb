c9c6142fa1dd86e83e896ee470e1cde55d2b345d
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-07-19 23:06:43 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,26 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  @@all = []
+
+  def initialize(name, owner)
+    @NAME = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def name
+    @NAME
+  end
+
+  def mood
+    if self.owner == nil
+     mood = "nervous"
+   end
+    @mood
+  end
+
+  def self.all
+    @@all
+  end
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,28 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, :mood
+  @@all = []
+
+  def initialize(name, owner)
+    @NAME = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def name
+    @NAME
+  end
+
+  def owner
+    @owner
+  end
+
+  def mood
+    #if self.owner == nil mood = "nervous"
+    @mood
+  end
+
+  def self.all
+    @@all
+  end
 end
